ee2af590fdfcbf313d6f55b0f12ea11eaf0fdeb7
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-12-20 21:51:23 +0500

    issues fixed

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,12 +1,12 @@
-import { BSON, Realm } from "realm";
+import Realm, { BSON } from "realm";
 
 export class Task extends Realm.Object<Task> {
-  _id!: BSON.ObjectId; // required
-  description!: string; // required
-  isComplete: boolean = false; // optional with default
-  createdAt: Date = new Date(); // optional with default
-  position: Realm.Types.Int = 0; // optional with default
-  user_id!: string; // required
+  _id: BSON.ObjectId = new BSON.ObjectId();
+  description!: string;
+  isComplete: boolean = false;
+  createdAt: Date = new Date();
+  position: Realm.Types.Int = 0;
+  user_id!: string;
 
-  static primaryKey = "_id"; // keep primary key
+  static primaryKey = "_id";
 }
