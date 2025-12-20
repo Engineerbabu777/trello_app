5a9a1ddeef1000275bb9096a8d4bcad1970db739
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-12-20 21:08:54 +0500

    add(Task): this new model has been added

---
+++ src/models/Task.ts
@@ -0,0 +1,14 @@
+import { BSON, Realm } from "realm";
+
+export class Task extends Realm.Object<Task> {
+  _id: BSON.ObjectId = new BSON.ObjectId();
+  description!: string;
+  isComplete: boolean = false;
+  createdAt: Date = new Date();
+
+  position: Realm.Types.Int = 0;
+
+  user_id!: string;
+
+  static primaryKey = "_id";
+}
