a5a5b22a4c693d0c7d6d8673fc8a3b5be7079727
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-12-20 19:14:23 +0500

    add(TaskList): has been re-udpated

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,11 +1,18 @@
+import { Text, View } from "react-native";
 import TaskListItem from "./TaskListItem";
 
 export default function TaskList() {
   return (
     <>
-      <TaskListItem task={{ description: "First Task" }} />
-      <TaskListItem task={{ description: "Second Task" }} />
-      <TaskListItem task={{ description: "Third Task" }} />
+      <View style={{}}>
+        <Text style={{}}>Todo</Text>
+
+        <TaskListItem task={{ description: "First Task" }} />
+        <TaskListItem task={{ description: "Second Task" }} />
+        <TaskListItem task={{ description: "Third Task" }} />
+
+        {/* INPUT */}
+      </View>
     </>
   );
 }
