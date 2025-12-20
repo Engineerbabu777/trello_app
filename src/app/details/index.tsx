794228d3bb7f02b448bebf6a5b139bd4dfb88f8b
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-12-20 22:07:00 +0500

    add(TaskDetailsScreen): this has been updated

--- src/app/details/index.tsx
+++ src/app/details/index.tsx
@@ -1,8 +1,17 @@
+import { Task } from "@/models/Task";
+import { useObject } from "@realm/react";
 import { Stack, useLocalSearchParams } from "expo-router";
 import { Text, View } from "react-native";
+import { BSON } from "realm";
 
 export default function TaskDetailsScreen() {
   const { id } = useLocalSearchParams();
+
+  const task = useObject<Task>(Task, new BSON.ObjectID(id as string));
+
+  if (!task) {
+    return <Text>Not found</Text>;
+  }
 
   return (
     <>
