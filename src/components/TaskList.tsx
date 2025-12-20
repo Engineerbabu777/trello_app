7841d8c5e26bcb3e26ed292b08fde9c202c5497f
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-12-20 19:19:00 +0500

    add(TaskList): has been updated with styles

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,15 +1,34 @@
-import { Text, View } from "react-native";
+import { FlatList, Text, View } from "react-native";
 import TaskListItem from "./TaskListItem";
+import { useState } from "react";
 
 export default function TaskList() {
+  const [tasks, setTasks] = useState([
+    { description: "First Task" },
+    { description: "Second Task" },
+  ]);
+
   return (
     <>
-      <View style={{}}>
-        <Text style={{}}>Todo</Text>
+      <View
+        style={{ backgroundColor: "#101112", padding: 10, borderRadius: 5 }}
+      >
+        <Text
+          style={{
+            color: "white",
+            fontWeight: "bold",
+            fontSize: 20,
+            marginVertical: 10,
+          }}
+        >
+          Todo
+        </Text>
 
-        <TaskListItem task={{ description: "First Task" }} />
-        <TaskListItem task={{ description: "Second Task" }} />
-        <TaskListItem task={{ description: "Third Task" }} />
+        <FlatList
+          contentContainerStyle={{ gap: 5 }}
+          data={tasks}
+          renderItem={({ item }) => <TaskListItem task={item} />}
+        />
 
         {/* INPUT */}
       </View>
